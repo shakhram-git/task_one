1fd3d1af17c85e43058929a0650dbbec978c1206
Shakhram <61085162+shakhram-git@users.noreply.github.com>
2022-08-04 13:22:30 +0600

    Task 1 finish

---
+++ app/src/main/java/com/example/myapplication/Playground.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+import java.lang.IllegalArgumentException
+
+fun main() {
+    val prices = mutableListOf<Int>(5,100,20,66,16)
+    val result = decryptData(prices, 50, 1, 3)
+    println(result)
+
+}
+
+fun decryptData(
+    prices: MutableList<Int>,
+    discount: Int,
+    offset: Int,
+    readLength: Int
+): List<Int> {
+    if (prices.any { it <= 0 }) throw IllegalArgumentException("Prices should be positive")
+    if (discount < 1 || discount > 99) throw IllegalArgumentException("Discount should be beyond 1 to 99")
+    if (offset < 0) throw IllegalArgumentException("Offset should be positive or zero")
+    if (readLength <= 0) throw IllegalArgumentException("Read length should be positive")
+    val result = prices.drop(1).take(3).map {
+        it * discount / 100
+    }
+    return result
+}
+
+
